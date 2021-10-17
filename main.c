60c374a585a6dd8b775aae940fb5861de158f2fe
ghazali <aymenghazali1999@gmail.com>
2021-10-18 00:03:25 +0100

    store data

--- main.c
+++ main.c
@@ -3,44 +3,79 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayghazal <ayghazal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ayghazal <ayghazal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 08:46:00 by ayghazal          #+#    #+#             */
-/*   Updated: 2021/10/17 16:39:31 by ayghazal         ###   ########.fr       */
+/*   Updated: 2021/10/18 00:02:24 by ayghazal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int c = 0;
-pthread_mutex_t mutex;
-
-void	*routine()
+void	print_data(t_data data)
 {
-	for(int i = 0; i < 10000000; i++)
-	{
-		pthread_mutex_lock(&mutex);
-		c++;
-		pthread_mutex_unlock(&mutex);
-	}
-
-	return NULL;
-	
+	printf("num_of_philo = %d\ntime_to_die = %lld\ntime_to_eat = %lld\ntime_to_sleep = %lld\nnum_of_meals = %d\n", data.num_of_philo, data.time_to_die, data.time_to_eat, data.time_to_sleep, data.num_of_meals);
 }
 
-int     main()
-{
-	pthread_t t1, t2;
-	pthread_mutex_init(&mutex, NULL);
-	
-	pthread_create(&t1, NULL, &routine, NULL);
-	
-	pthread_create(&t2, NULL, &routine, NULL);
-	
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
 
-	printf("c = %d\n", c);
-	pthread_mutex_destroy(&mutex);
+int		ft_onlydigit(char *s)
+{
+	int i;
+
+	i = 0;
+	while(s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			i++;
+		else
+			return(1);
+	}
+	return(0);
+}
+
+int		check_data(char **str)
+{
+	int i;
+	
+	i = 0;
+	while(str[i])
+		i++;
+	if (i == 5 || i == 6)
+	{
+		i = 1;
+		while (str[i])
+		{
+			if (ft_onlydigit(str[i]) == 1)
+				return(1);
+			i++;
+		}
+	}
+	else
+		return(1);
+	return(0);
+}
+
+void		store_data(char **str, t_data *data)
+{
+
+	data->num_of_philo = ft_atoi(str[1]);
+	data->time_to_die = ft_atoi(str[2]) * 1000;
+	data->time_to_eat = ft_atoi(str[3]) * 1000;
+	data->time_to_sleep = ft_atoi(str[4]) * 1000;
+	if (str[5] != NULL)
+		data->num_of_meals = ft_atoi(str[5]);
+}
+
+int	main(int ac, char *av[])
+{
+	t_data data;
+
+	if (check_data(av) == 0)
+	{
+		store_data(av, &data);
+		print_data(data);
+	}
+	else
+		printf("Error: Arguments!!\n");
 	return(0);
 }
--- philo.h
+++ philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayghazal <ayghazal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ayghazal <ayghazal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 16:38:46 by ayghazal          #+#    #+#             */
-/*   Updated: 2021/10/17 16:39:16 by ayghazal         ###   ########.fr       */
+/*   Updated: 2021/10/17 23:55:13 by ayghazal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,23 @@
 #include "utils/utill.h"
 #include <pthread.h>
 
+typedef struct s_data{
+    
+    int num_of_philo;
+    long long time_to_die;
+    long  long time_to_eat;
+    long long time_to_sleep;
+    int num_of_meals;
+    
+} t_data;
+
+/*typedef struct s_philo{
+    //thread
+    //mutex
+    //last time ate
+    // pointer to data 
+    t_data *data;
+    struct s_philo *next;
+} t_philo;*/
 
 #endif
