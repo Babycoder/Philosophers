9ac3a7f981b5f6b53623c043570166843ba3db76
Aymen Ghazali <ayghazal@e1r6p4.1337.ma>
2021-10-17 16:53:24 +0100

    start

--- main.c
+++ main.c
@@ -3,29 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayghazal <ayghazal@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: ayghazal <ayghazal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 08:46:00 by ayghazal          #+#    #+#             */
-/*   Updated: 2021/10/14 15:09:25 by ayghazal         ###   ########.fr       */
+/*   Updated: 2021/10/17 16:39:31 by ayghazal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include "utils/utill.h"
-#include <pthread.h>
+#include "philo.h"
+
+int c = 0;
+pthread_mutex_t mutex;
 
 void	*routine()
 {
-	printf("hello thread\n");
+	for(int i = 0; i < 10000000; i++)
+	{
+		pthread_mutex_lock(&mutex);
+		c++;
+		pthread_mutex_unlock(&mutex);
+	}
+
+	return NULL;
+	
 }
 
 int     main()
 {
-	/*pthread_t t1;
+	pthread_t t1, t2;
+	pthread_mutex_init(&mutex, NULL);
+	
 	pthread_create(&t1, NULL, &routine, NULL);
-	pthread_join(t1, NULL);*/
-	char s[] = "aymen";
-	int len = (int)ft_strlen(s);
-	printf("len = %d\n", len);
+	
+	pthread_create(&t2, NULL, &routine, NULL);
+	
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+
+	printf("c = %d\n", c);
+	pthread_mutex_destroy(&mutex);
 	return(0);
 }
